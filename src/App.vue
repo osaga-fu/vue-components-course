831e7d2369a77a1db1ff22b91bab0d673021ed63
Olaya Sánchez <olaya.sanchez@vml.com>
2025-04-07 11:29:46 +0200

    Create PlanPicker component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,5 @@
 <script setup>
-import { ref } from 'vue'
-import CoffeePlan from './components/CoffeePlan.vue'
-
-const plans = ref([
-  { name: 'The Hacker', price: 10 },
-  { name: 'The Single', price: 8 },
-  { name: 'The Double', price: 15 },
-])
+import PlanPicker from './components/PlanPicker.vue'
 </script>
 
 <template>
@@ -17,9 +10,7 @@ const plans = ref([
       We travel the world to source the very best single origin coffee for you
     </h2>
 
-    <div class="plans">
-      <CoffeePlan v-for="plan in plans" :key="plan" :name="plan.name" :price="plan.price" />
-    </div>
+    <PlanPicker />
   </div>
 </template>
 
---
+++ src/components/PlanPicker.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue';
+import CoffeePlan from './CoffeePlan.vue';
+
+const plans = ref([
+  { name: 'The Hacker', price: 10 },
+  { name: 'The Single', price: 8 },
+  { name: 'The Double', price: 15 },
+])
+</script>
+
+<template>
+    <div class="plans">
+      <CoffeePlan v-for="plan in plans" :key="plan" :name="plan.name" :price="plan.price" />
+    </div>
+</template>
+
+<style scoped>
+
+</style>
