e2347e70365e968463e6426ad635f4fa6a74c733
Olaya Sánchez <olaya.sanchez@vml.com>
2025-04-07 11:08:54 +0200

    Create CoffeePlan component with a prop

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <script setup>
-import CounterButton from './components/CounterButton.vue'
+import CoffeePlan from './components/CoffeePlan.vue';
+
+
 </script>
 
 <template>
-  <CounterButton />
+  <div class="content">
+    <h1 class="title">Coffee Plans</h1>
+
+    <h2 class="subtitle">
+      We travel the world to source the very best single origin coffee for you
+    </h2>
+
+    <div class="plans">
+      <CoffeePlan name="The Hacker" />
+      <CoffeePlan name="The Single" />
+      <CoffeePlan name="The Double" />
+    </div>
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
---
+++ src/components/CoffeePlan.vue
@@ -0,0 +1,13 @@
+<script setup>
+defineProps(['name'])
+</script>
+
+<template>
+  <div class="plan">
+    <div class="description">
+      <span class="title">{{ name }}</span>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
